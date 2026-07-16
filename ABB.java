b3b1bb07f93b6fc26d1d587be2b4d5aa1be9013f
Igor Ferreira <igor14022004@outlook.com>
2026-07-16 15:39:25 -0300

    Alunos completo

--- ABB.java
+++ ABB.java
@@ -1,33 +1,357 @@
+// Classe ABB para demonstrar a inserção, busca, remoção, atravessamentos etc.
+// em uma Árvore Binária de Busca (ABB).
+// Ledón, 2016/2017; Amilton Souza Martha, 2015/2017.
 
+package testa_abb;
 
-public class ABB {
-    private NoABB raiz;
+import java.util.LinkedList;
 
-    public void inserir(Aluno aluno) {
-        raiz = inserirRec(raiz, aluno);
+class ABB<E extends Comparable<E>> {  // Árvore Binária de Busca 
+
+    private Node raiz;
+
+    public ABB() {
+        raiz = null;
     }
 
-    private NoABB inserirRec(NoABB no, Aluno aluno) {
-        if (no == null) {
-            return new NoABB(aluno);
+    public boolean isEmpty() {
+        return (raiz == null);
+    }
+
+    //Configura a raiz da árvore
+    public void setRaiz(Node araiz) {
+        raiz = araiz;
+    }
+
+    public Node getRaiz() {
+        return raiz;
+    }
+
+    public E inserir(E valor) {
+        try {
+            Node novo = new Node(valor);
+            this.inserir(novo, raiz);
+        } catch (Exception exMemoria) {
+            return null;
+        }   // memória insuficiente
+        return (valor);
+    }
+
+    /*
+      O Método 'inserir' utiliza o método 'compareTo', que deverá existir na classe de objetos a inserir na ABB.
+      O método 'compareTo' da interface Comparable (implementado nas classes Double, Integer, String etc.) lançará 
+      uma exception se são comparados objetos de tipos diferentes.
+      Consequentemente, este método 'inserir' só conseguirá adicionar novos nodos que guardem objetos do mesmo tipo
+      que o objeto inicial guardado na ABB, ou seja, a ABB guardará objetos do mesmo tipo.
+    */
+    private Node inserir(Node novo, Node anterior) {
+        if (raiz == null) {    // ou if(isEmpty())
+            raiz = novo;  // ou setRaiz(novo);
+            return raiz;
         }
-        if (aluno.compareTo(no.aluno) < 0) {
-            no.esquerda = inserirRec(no.esquerda, aluno);
+        if (anterior == null) {
+            return novo; // chegou em uma folha
         } else {
-            no.direita = inserirRec(no.direita, aluno);
+            if (novo.getValue().compareTo(anterior.getValue()) < 0) {
+                anterior.setFilhoEsquerdo(inserir(novo, anterior.getFilhoEsquerdo()));
+            } else {
+                anterior.setFilhoDireito(inserir(novo, anterior.getFilhoDireito()));
+            }
+            return anterior;
         }
-        return no;
     }
 
     public void emOrdem() {
-        emOrdemRec(raiz);
+        emOrdem(raiz);
     }
 
-    private void emOrdemRec(NoABB no) {
+    public void emOrdem2() {
+        emOrdem2(raiz);
+    }
+
+    public void preOrdem() {
+        preOrdem(raiz);
+    }
+
+    public void posOrdem() {
+        posOrdem(raiz);
+    }
+
+    public void emOrdem(Node no) {
         if (no != null) {
-            emOrdemRec(no.esquerda);
-            System.out.println(no.aluno);
-            emOrdemRec(no.direita);
+            emOrdem(no.getFilhoEsquerdo());
+            System.out.print(no.getValue() + "   ");
+            emOrdem(no.getFilhoDireito());
         }
     }
+
+    public void emOrdem2(Node no) {
+        if (no != null) {
+            emOrdem2(no.getFilhoEsquerdo());
+            System.out.print(no.getValue() + "\n");
+            emOrdem2(no.getFilhoDireito());
+        }
+    }
+
+    public void preOrdem(Node no) {
+        if (no != null) {
+            System.out.print(no.getValue() + "   ");
+            preOrdem(no.getFilhoEsquerdo());
+            preOrdem(no.getFilhoDireito());
+        }
+    }
+
+    public void posOrdem(Node no) {
+        if (no != null) {
+            posOrdem(no.getFilhoEsquerdo());
+            posOrdem(no.getFilhoDireito());
+            System.out.print(no.getValue() + "   ");
+        }
+    }
+
+    public void emNivel() {
+        //Método iterativo que utiliza uma fila auxiliar
+        Node noAux;
+        LinkedList fila = new LinkedList();
+        fila.addLast(raiz);
+        while (!fila.isEmpty()) {
+            noAux = (Node) fila.removeFirst();
+            if (noAux.getFilhoEsquerdo() != null) {
+                fila.addLast(noAux.getFilhoEsquerdo());
+            }
+            if (noAux.getFilhoDireito() != null) {
+                fila.addLast(noAux.getFilhoDireito());
+            }
+            System.out.print(noAux.getValue() + "   ");
+        }
+    }
+
+        public float mediaDasMulheres() {
+        return mediaDasMulheres(raiz)[0]; // índice 0 = média calculada
+    }
+
+    // Retorna um array de float: [média, soma, quantidade]
+    private float[] mediaDasMulheres(Node node) {
+        float soma = 0;
+        int cont = 0;
+
+        if (node != null) {
+            float[] esq = mediaDasMulheres(node.getFilhoEsquerdo());
+            float[] dir = mediaDasMulheres(node.getFilhoDireito());
+
+            Object valor = node.getValue();
+            if (valor instanceof Aluno) {
+                Aluno a = (Aluno) valor;
+                if (a.getSexo() == 'F') {
+                    soma += a.getMedia();
+                    cont++;
+                }
+            }
+
+            soma += esq[1] + dir[1];
+            cont += esq[2] + dir[2];
+        }
+
+        float media = (cont > 0) ? soma / cont : 0;
+        return new float[]{media, soma, cont};
+    }
+    
+    //Determina o menor elemento a partir de um nó
+    public Node getMenor(Node node) {
+        if (isEmpty()) {
+            return null;
+        }
+        if (node.getFilhoEsquerdo() == null) {
+            return node;
+        } else {
+            return getMenor(node.getFilhoEsquerdo());
+        }
+    }
+
+    //Determina o maior elemento a partir de um nó
+    public Node getMaior(Node node) {
+        if (isEmpty()) {
+            return null;
+        }
+        if (node.getFilhoDireito() == null) {
+            return node;
+        } else {
+            return getMaior(node.getFilhoDireito());
+        }
+    }
+
+    //Determina o maior elemento a partir de um nó 'raiz' 
+    //(e enlaça seu pai para eliminar esse nodo 'raiz' desta posição).
+    //Retorna o nodo com maior valor desta subárvore.
+    public Node getMax(Node raiz, Node paiRaiz) {
+        if (isEmpty()) {
+            return null;
+        }
+        Node aux;
+        //Se não tiver mais filho direito
+        if (raiz.getFilhoDireito() == null) { //encontrou o maior
+            aux = raiz;
+            //Se tiver um pai, ele assume o filho esquerdo (nunca terá filho direito)
+            if (paiRaiz != null) {
+                if (paiRaiz.getFilhoEsquerdo() == raiz) { //se 'raiz' era filho esquerdo do pai
+                    paiRaiz.setFilhoEsquerdo(raiz.getFilhoEsquerdo());
+                } else {  //se 'raiz' era filho direito do pai
+                    paiRaiz.setFilhoDireito(raiz.getFilhoEsquerdo());
+                }
+            }
+            return aux;
+        } else {
+            return getMax(raiz.getFilhoDireito(), raiz);
+        }
+    }
+
+    private int compara(Object ob1, Object ob2) {
+        return ((Comparable) ob1).compareTo(((Comparable) ob2));
+    }
+
+     public boolean eliminar(Object e) {
+        return eliminar(raiz, null, e);
+    }
+
+    private boolean eliminar(Node node, Node paiRaiz, Object e) {  // remove um elemento da árvore, retorna true ou false
+        Node aux;
+        if (node == null) {  // não achou o elemento, não existe (chegou em uma folha, ou árvore vazia)
+                return false;  // abandonamos o método retornando false, não foi possível eliminar
+        } else { // a árvore ou sub-árvore não está vazia:
+            if (compara(e, node.getValue()) == 0) {  // se o nó a eliminar, node, foi encontrado (ele guarda o objeto e)
+                aux = node;
+                if (node.getFilhoEsquerdo() == null && node.getFilhoDireito() == null) {  // caso 1: se node não possui filhos, basta eliminar o nó
+                            if (paiRaiz == null) {  // se o node a eliminar não tiver pai, ele era a raiz da árvore, então a árvore ficou vazia
+                                setRaiz(null);  // convenção para ABB vazia
+                            } 
+                            else {  // senão, o pai deve "deserdar" o filho (ficar sem esse filho eliminado)
+                                // verifica se o nó que será eliminado é o filho esquerdo ou direito  do pai:
+                                if (paiRaiz.getFilhoEsquerdo() != null && compara(paiRaiz.getFilhoEsquerdo().getValue(), e) == 0) {
+                                    paiRaiz.setFilhoEsquerdo(null);
+                                } else if (paiRaiz.getFilhoDireito() != null && compara(paiRaiz.getFilhoDireito().getValue(), e) == 0) {
+                                    paiRaiz.setFilhoDireito(null);
+                                }
+                            }
+                } else if (node.getFilhoDireito() == null) {   // caso 2a: se node só tiver o filho esquerdo
+                            if (paiRaiz != null) {  // se node tiver um pai, o pai (paiRaiz) assume o filho esquerdo de node
+                                // verifica se a raiz é filho esquerdo ou direito de paiRaiz, para assumir o neto:
+                                if (paiRaiz.getFilhoEsquerdo() != null && compara(paiRaiz.getFilhoEsquerdo().getValue(), e) == 0) {
+                                    paiRaiz.setFilhoEsquerdo(node.getFilhoEsquerdo());
+                                } else {
+                                    paiRaiz.setFilhoDireito(node.getFilhoEsquerdo());
+                                }
+                            } 
+                            else { // se node não tiver pai (caso da raiz da árvore, paiRaiz é nulo), adotar seu filho ou mover a raiz:
+                                node.setValue(node.getFilhoEsquerdo().getValue());
+                                raiz = raiz.getFilhoEsquerdo();  // mover a raiz da árvore
+                                //ou adotar o filho:
+                                //node.setFilhoEsquerdo(node.getFilhoEsquerdo().getFilhoEsquerdo());
+                                //node.setFilhoDireito(node.getFilhoEsquerdo().getFilhoDireito());
+                            }
+                } else if (node.getFilhoEsquerdo() == null) {   // caso 2b: se node só tiver o filho direito                    
+                        if (paiRaiz != null) {  //se node tiver um pai, o pai (paiRaiz) assume o filho direito de node:
+                            // verifica se a raiz paiRaiz tem node como filho esquerdo ou direito, para assumir o neto:
+                            if (paiRaiz.getFilhoEsquerdo() != null && compara(paiRaiz.getFilhoEsquerdo().getValue(), e) == 0) {
+                                paiRaiz.setFilhoEsquerdo(node.getFilhoDireito());
+                            } else {
+                                paiRaiz.setFilhoDireito(node.getFilhoDireito());
+                            }
+                        } 
+                        else {  // se node não tiver pai (caso da raiz da árvore, paiRaiz é nulo), adotar seu filho ou mover a raiz:
+                            node.setValue(node.getFilhoDireito().getValue());
+                            raiz = raiz.getFilhoDireito();  // mover a raiz da árvore
+                            //ou adotar o filho:
+                            //node.setFilhoEsquerdo(node.getFilhoDireito().getFilhoEsquerdo());
+                            //node.setFilhoDireito(node.getFilhoDireito().getFilhoDireito());
+                       }
+                } else {   //caso 3: o nodo node possui os dois filhos:
+                            aux = getMax(node.getFilhoEsquerdo(), node); //determina o maior da subárvore esquerda
+                            node.setValue(aux.getValue());
+                 }
+                aux = null;
+                return true;  // fim dos casos em que o nó a eliminar foi encontrado, retornamos true
+            } else { // se node não é o nó a eliminar, continuamos procurando recursivamente:
+                    if (compara(e, node.getValue()) < 0) { // se o objeto e for menor que o objeto em node, continuar procurando à esquerda:
+                        return eliminar(node.getFilhoEsquerdo(), node, e);  // chamada recursiva
+                    } else { // senão, procurar à direita:
+                        return eliminar(node.getFilhoDireito(), node, e);  // chamada recursiva
+                    }
+            }
+        }
+    }    
+
+    public Node searchABB(Object obj) {
+        return searchABB(raiz, obj);
+    }
+
+    //Busca um elemento na árvore e retorna o nó onde encontrou ou null se não encontrou o nodo
+    private Node searchABB(Node node, Object obj) { // procura obj a partir do nó node
+        //Se a raiz for nula (árvore vazia) ou chegou em uma folha => não achou o objeto procurado:
+        if (node == null) { // ou   if (isEmpty()) 
+            return null;
+        } else {
+            //Se achou o elemento, retornar o nó:
+            if (((Comparable) obj).compareTo(node.getValue()) == 0) {
+                return node;
+            } else {
+                //Se não achou, buscar recursivamente para a esquerda ou direita:
+                if (((Comparable) obj).compareTo(node.getValue()) < 0) {
+                    return searchABB(node.getFilhoEsquerdo(), obj);
+                } else {
+                    return searchABB(node.getFilhoDireito(), obj);
+                }
+            }
+        }
+    }
+
+    // Algumas implementações de operações com ABBs em forma iterativa:
+    
+    public Node find(Object obj) {  // método (iterativo) para buscar um obj na ABB                          
+        if(isEmpty()) return null;
+        Node atual = raiz; // começamos na raiz
+        while (compara(obj, atual.getValue()) != 0) { // enquanto não encontremos o item procurado
+            if (compara(obj, atual.getValue()) < 0) {
+                atual = atual.getFilhoEsquerdo();
+            } else {
+                atual = atual.getFilhoDireito();
+            }
+            if (atual == null) {
+                return null;  // retornamos null se não encontramos o item procurado               
+            }
+        }
+        return atual; // retornamos o nodo encontrado
+    }
+
+    public E insert(E valor) { // implementação iterativa da inserção na ABB
+        Node novoNodo;
+        try {
+            novoNodo = new Node(valor);
+        } catch (Exception ex) {
+            return null;
+        } // memória insuficiente
+        if (isEmpty()) {
+            raiz = novoNodo; // se a ABB estiver vazia, inserimos na raiz 
+        } else {
+            Node atual = raiz;  // começamos procurar pela raiz      
+            Node pai;
+            while (true) {  // ciclo que só será interrompido ao acontecer a inserção (break interno)
+                pai = atual;
+                if (compara(valor, atual.getValue()) < 0) { // verificamos se devemos ir para a esquerda
+                    atual = atual.getFilhoEsquerdo();
+                    if (atual == null) {  // inserir à esquerda
+                        pai.setFilhoEsquerdo(novoNodo);
+                        break;
+                    }
+                } else { // ou ir para direita
+                    atual = atual.getFilhoDireito();
+                    if (atual == null) { // inserir a direita
+                        pai.setFilhoDireito(novoNodo);
+                        break;
+                    }
+                }
+            }
+        }
+        return valor;
+    }
+
 }
--- Aluno.java
+++ Aluno.java
@@ -1,27 +1,70 @@
-public class Aluno implements Comparable<Aluno> {
-    private String nome;
-    private int matricula;
+package testa_abb;
 
-    public Aluno(String nome, int matricula) {
+public class Aluno implements Comparable<Aluno> {
+    private String rgm, nome;
+    private char sexo;
+    private float media;
+
+    public Aluno(String rgm, String nome, char sexo, float media) {
+        this.rgm = rgm;
         this.nome = nome;
-        this.matricula = matricula;
+        this.sexo = sexo;
+        this.media = media;
+    }
+    
+    public String toString() {
+        return rgm + " - " + nome + ", sexo: " + sexo + ", média: " + media;
+    }
+
+    public String getRgm() {
+        return rgm;
+    }
+
+    public void setRgm(String rgm) {
+        this.rgm = rgm;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public int getMatricula() {
-        return matricula;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
     }
 
     @Override
-    public int compareTo(Aluno outro) {
-        return this.nome.compareToIgnoreCase(outro.nome);
+    public int compareTo(Aluno outro) {  // comparamos os RGMs
+        if(rgm.compareTo(outro.getRgm()) < 0)return -1;
+        else if(rgm.compareTo(outro.getRgm()) == 0)return 0;
+        else return  1;
     }
-
+    
+    /*
     @Override
-    public String toString() {
-        return nome + " (" + matricula + ")";
-    }
+    public int compareTo(Aluno outro) {  // nesta versão comparamos as médias
+        if(media < outro.getMedia())return -1;
+        else if(media == outro.getMedia())return 0;
+        else return  1;
+    }    
+    */
+    
 }
